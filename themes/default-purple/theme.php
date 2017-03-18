984cf6b74257145201f1775cc2dc11859373faff
robiso <r@robiso.com>
2017-03-18 17:36:07 +0100

    Update theme.php

--- themes/default-purple/theme.php
+++ themes/default-purple/theme.php
@@ -4,44 +4,45 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?=$siteTitle?> - <?=$title?></title>
-	<meta name="description" content="<?=$description?>">
-	<meta name="keywords" content="<?=$keywords?>">
+	<title><?=wCMS::get('config','siteTitle')?> - <?=wCMS::page('title')?></title>
+	<meta name="description" content="<?=wCMS::page('description')?>">
+	<meta name="keywords" content="<?=wCMS::page('keywords')?>">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?=wCMS::asset('css/style.css')?>">
 	<?=wCMS::css()?>
 </head>
 <body>
-	<?=wCMS::displayMessages()?>
+	<?=wCMS::alerts()?>
 	<?=wCMS::settings()?>
 	<nav class="navbar navbar-default">
 		<div class="container">
 			<div class="col-sm-5 text-center">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navMobile">&#9776;</button>
-					<a href="./"><h1><?=$siteTitle?></h1></a>
+					<a href="./"><h1><?=wCMS::get('config','siteTitle')?></h1></a>
 				</div>
 			</div>
 			<div class="col-sm-7 text-center">
 				<div class="collapse navbar-collapse" id="navMobile">
 					<ul class="nav navbar-nav navbar-right">
-						<?=wCMS::navigation()?>
+						<?=wCMS::menu()?>
 					</ul>
 				</div>
 			</div>
 		</div>
 	</nav>
 
+
 	<div class="container">
 		<div class="col-xs-12 col-sm-8">
 			<div class="whiteBackground grayFont padding20 rounded5">
-				<?=$content?>
+				<?=wCMS::page('content')?>
 			</div>
 		</div>
 		<div class="col-xs-12 col-sm-4">
 			<div class="visible-xs spacer20"></div>
 			<div class="purpleBackground padding20 rounded5">
-				<?=$subside?>
+				<?=wCMS::block('subside')?>
 			</div>
 		</div>
 	</div>
