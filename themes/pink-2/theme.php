5449c1e7fcf6ae9339d2676d6de95827eac3c7af
robiso <r@robiso.com>
2017-08-22 20:45:26 +0200

    Update theme.php

--- themes/pink-2/theme.php
+++ themes/pink-2/theme.php
@@ -54,7 +54,6 @@
 	</footer>
 
 	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/jquery.autosize/3.0.17/autosize.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<?=wCMS::js()?>
 </body>
