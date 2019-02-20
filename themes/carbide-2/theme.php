c348c6cc07736fbcba1e3d2e508b585779a9e6db
Robert Isoski <r@robiso.com>
2019-02-20 18:42:28 +0100

    Update theme.php
    Updated theme SRI tags, fixed spacing/tabs.

--- themes/carbide-2/theme.php
+++ themes/carbide-2/theme.php
@@ -1,136 +1,141 @@
 <!DOCTYPE html>
 <?php
-if(defined('VERSION') && !defined('version'))
-	define('version', VERSION);
-if(version<'2.0.0')
-	defined('INC_ROOT') OR die('Direct access is not allowed.');
+    if(defined('VERSION') && !defined('version'))
+    	define('version', VERSION);
+    if(version<'2.0.0')
+    	defined('INC_ROOT') OR die('Direct access is not allowed.');
 
-wCMS::addListener('menu', 'colorSelector');
+    wCMS::addListener('menu', 'colorSelector');
 
-function colorSelector ($args) {
-	$args[0] .= '
-<li>
-	<ul class="color-selector">
-		<li><a href="" id="nightmode"><span class="glyphicon glyphicon-adjust" style="color: black;" aria-hidden="true"></span></a></li><li><a href="" id="default"><span class="glyphicon glyphicon-adjust" style="color: white;" aria-hidden="true"></span></a></li>
-	    <li><p><font size="-8">Theme</font></p></li>
-	</ul>
-</li>';
-	return $args;
-}
-if(isset($_COOKIE['stylesheet'])) {
-	switch($_COOKIE['stylesheet']) {
-		case 'nightmode':
-			$css = 'css/style-night.css';
-			break;
-		default:
-			$css = 'css/style-default.css';
-			break;
-	}
-} else {
-	$css = 'css/style-default.css';
-}
+    function colorSelector ($args) {
+    	$args[0] .= '
+            <li>
+            	<ul class="color-selector">
+            		<li><a href="" id="nightmode"><span class="glyphicon glyphicon-adjust" style="color: black;" aria-hidden="true"></span></a></li>
+            		<li><a href="" id="default"><span class="glyphicon glyphicon-adjust" style="color: white;" aria-hidden="true"></span></a></li>
+            	    <li><p><font size="-8">Theme</font></p></li>
+            	</ul>
+            </li>';
+    	return $args;
+    }
+
+    if(isset($_COOKIE['stylesheet'])) {
+        switch($_COOKIE['stylesheet']) {
+        	case 'nightmode':
+        		$css = 'css/style-night.css';
+        		break;
+        	default:
+        		$css = 'css/style-default.css';
+        		break;
+        }
+    } else {
+        $css = 'css/style-default.css';
+    }
 ?>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="theme-color" content="#75b1f2">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?=wCMS::get('config','siteTitle')?> - <?=wCMS::page('title')?></title>
-	<meta name="description" content="<?=wCMS::page('description')?>">
-	<meta name="keywords" content="<?=wCMS::page('keywords')?>">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link id="stylesheet" rel="stylesheet" href="<?=wCMS::asset($css)?>">
-	<?=wCMS::css()?>
+    <head>
+    	<meta charset="UTF-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="theme-color" content="#75b1f2">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-</head>
-<body>
-	<div style="margin-top:60px">
-	<?=wCMS::alerts()?>
-	<?=wCMS::settings()?>
-    </div>
+    	<title><?= wCMS::get('config','siteTitle') ?> - <?= wCMS::page('title') ?></title>
+    	<meta name="description" content="<?=wCMS::page('description') ?>">
+    	<meta name="keywords" content="<?= wCMS::page('keywords') ?>">
 
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container-fluid css3-shadow colorBackground">
-			<div class="navbar-header padLeft15">
-				<button type="button" class="navbar-toggle x collapsed" data-toggle="collapse" data-target="#navbar-collapse-x">
-					<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-				</button>
-					<a class="navbar-brand" href="<?= wCMS::url() ?>">
-						<?= wCMS::get('config','siteTitle') ?>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    	<link id="stylesheet" rel="stylesheet" href="<?= wCMS::asset($css) ?>">
+    	<?= wCMS::css() ?>
 
-					</a>
-			</div>
-			<div class="collapse navbar-collapse" id="navbar-collapse-x">
-				<ul class="nav navbar-nav navbar-right">
-					<?=wCMS::menu()?>
+    </head>
 
-				</ul>
-			</div>
-		</div>
-	</nav>
+    <body>
+    	<div style="margin-top:60px">
+        	<?= wCMS::alerts() ?>
+        	<?= wCMS::settings() ?>
 
-	<div class="container-fluid">
-		<div class="row-fluid sm-gutter">
-			<div class="col-xs-12 col-sm-8">
-			<div class="box css3-shadow whiteBackground padding10">
-				<?=wCMS::page('content')?>
-			</div>
-			</div>
+        </div>
 
-			<div class="col-xs-12 col-sm-4">
-			<div class="box css3-shadow whiteBackground padding10">
-				<?=wCMS::block('subside')?>
+    	<nav class="navbar navbar-inverse navbar-fixed-top">
+    		<div class="container-fluid css3-shadow colorBackground">
+    			<div class="navbar-header padLeft15">
+    				<button type="button" class="navbar-toggle x collapsed" data-toggle="collapse" data-target="#navbar-collapse-x">
+    					<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+    				</button>
+    					<a class="navbar-brand" href="<?= wCMS::url() ?>">
+    						<?= wCMS::get('config','siteTitle') ?>
 
+    					</a>
+    			</div>
+    			<div class="collapse navbar-collapse" id="navbar-collapse-x">
+    				<ul class="nav navbar-nav navbar-right">
+    					<?= wCMS::menu() ?>
 
-				<div class="wrapper">
-				<div class="links">
-                <a href="https://github.com/carbideband"><svg class="svg-icon" style="width:30px; height:25px; fill:#75b1f2;"><use xlink:href="/themes/carbide/img/minima-social-icons.svg#github"></use></svg></a>
-			    </div>
-			    </div>
+    				</ul>
+    			</div>
+    		</div>
+    	</nav>
 
+    	<div class="container-fluid">
+    		<div class="row-fluid sm-gutter">
+    			<div class="col-xs-12 col-sm-8">
+        			<div class="box css3-shadow whiteBackground padding10">
+        				<?= wCMS::page('content') ?>
 
-			</div>
-			</div>
+        			</div>
+    			</div>
 
-		</div>
-	</div>
+    			<div class="col-xs-12 col-sm-4">
+        			<div class="box css3-shadow whiteBackground padding10">
+        				<?= wCMS::block('subside') ?>
 
-	<footer class="container-fluid css3-shadow whiteFont colorBackground padding20">
-				<p><?= wCMS::footer() ?></p>
-	</footer>
-	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/jquery.autosize/3.0.17/autosize.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<?=wCMS::js()?>
-	<script src="<?=wCMS::asset('js/js.cookie.js')?>"></script>
-	<script>
-		$( document ).ready(function() {
-			$('body').css('margin-bottom', $('footer').height()+'px');
-			
-			function change_stylesheet(c) {
-				switch(c) {
-					case 'nightmode':
-						var stylesheet = $('#stylesheet').attr('href').replace(/css\/style\-(.*)/g, 'css/style-night.css');
-						break;
-					case 'default':
-						var stylesheet = $('#stylesheet').attr('href').replace(/css\/style\-(.*)/g, 'css/style-default.css');
-						break;
-				}
-				Cookies.set("stylesheet", c, { expires: 365 });
-				$("#stylesheet").attr({href: stylesheet});
-			}
-			
-			if(Cookies.get("stylesheet")) {
-				change_stylesheet(Cookies.get("stylesheet"));
-			}
-			
-			$('.color-selector li a').click(function(e){
-				e.preventDefault();
-				change_stylesheet($(this).attr('id'));
-			});
-		});
-	</script>
-</body>
+        				<div class="wrapper">
+            				<div class="links">
+                                <a href="https://github.com/carbideband">
+                                    <svg class="svg-icon" style="width:30px; height:25px; fill:#75b1f2;"><use xlink:href="/themes/carbide/img/minima-social-icons.svg#github"></use></svg>
+                                </a>
+            			    </div>
+        			    </div>
+        			</div>
+    			</div>
+    		</div>
+    	</div>
+
+    	<footer class="container-fluid css3-shadow whiteFont colorBackground padding20">
+    		<p><?= wCMS::footer() ?></p>
+    	</footer>
+
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    	<?= wCMS::js() ?>
+
+    	<script src="<?= wCMS::asset('js/js.cookie.js') ?>"></script>
+    	<script>
+    		$( document ).ready(function() {
+    			$('body').css('margin-bottom', $('footer').height()+'px');
+
+    			function change_stylesheet(c) {
+    				switch(c) {
+    					case 'nightmode':
+    						var stylesheet = $('#stylesheet').attr('href').replace(/css\/style\-(.*)/g, 'css/style-night.css');
+    						break;
+    					case 'default':
+    						var stylesheet = $('#stylesheet').attr('href').replace(/css\/style\-(.*)/g, 'css/style-default.css');
+    						break;
+    				}
+    				Cookies.set("stylesheet", c, { expires: 365 });
+    				$("#stylesheet").attr({href: stylesheet});
+    			}
+
+    			if(Cookies.get("stylesheet")) {
+    				change_stylesheet(Cookies.get("stylesheet"));
+    			}
+
+    			$('.color-selector li a').click(function(e){
+    				e.preventDefault();
+    				change_stylesheet($(this).attr('id'));
+    			});
+    		});
+    	</script>
+    </body>
 </html>
-
