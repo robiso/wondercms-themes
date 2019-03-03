b5ced55387dfd70ad97d8bee1fef8c48014e2fc4
Filthy Luis <notcarbide@users.noreply.github.com>
2019-03-03 15:48:17 -0600

    carbide: Update for wondercms 'dev' branch
    - Use global Wcms class
    - Use fontawesome

--- themes/carbide-2/theme.php
+++ themes/carbide-2/theme.php
@@ -1,18 +1,15 @@
+<?php global $Wcms ?>
 <!DOCTYPE html>
 <?php
-    if(defined('VERSION') && !defined('version'))
-    	define('version', VERSION);
-    if(version<'2.0.0')
-    	defined('INC_ROOT') OR die('Direct access is not allowed.');
 
-    wCMS::addListener('menu', 'colorSelector');
+    $Wcms->addListener('menu', 'colorSelector');
 
     function colorSelector ($args) {
     	$args[0] .= '
             <li>
             	<ul class="color-selector">
-            		<li><a href="" id="nightmode"><span class="glyphicon glyphicon-adjust" style="color: black;" aria-hidden="true"></span></a></li>
-            		<li><a href="" id="default"><span class="glyphicon glyphicon-adjust" style="color: white;" aria-hidden="true"></span></a></li>
+            		<li><a href="" id="nightmode"><span class="fa fa-moon-o fa-lg" style="color: black;" aria-hidden="true"></span></a></li>
+            		<li><a href="" id="default"><span class="fa fa-sun-o fa-lg" style="color: white;" aria-hidden="true"></span></a></li>
             	    <li><p><font size="-8">Theme</font></p></li>
             	</ul>
             </li>';
@@ -39,20 +36,21 @@
         <meta name="theme-color" content="#75b1f2">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    	<title><?= wCMS::get('config','siteTitle') ?> - <?= wCMS::page('title') ?></title>
-    	<meta name="description" content="<?=wCMS::page('description') ?>">
-    	<meta name="keywords" content="<?= wCMS::page('keywords') ?>">
+    	<title><?= $Wcms->get('config','siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+    	<meta name="description" content="<?=$Wcms->page('description') ?>">
+    	<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    	<link id="stylesheet" rel="stylesheet" href="<?= wCMS::asset($css) ?>">
-    	<?= wCMS::css() ?>
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    	<link id="stylesheet" rel="stylesheet" href="<?= $Wcms->asset($css) ?>">
+    	<?= $Wcms->css() ?>
 
     </head>
 
     <body>
     	<div style="margin-top:60px">
-        	<?= wCMS::alerts() ?>
-        	<?= wCMS::settings() ?>
+        	<?= $Wcms->alerts() ?>
+        	<?= $Wcms->settings() ?>
 
         </div>
 
@@ -62,14 +60,14 @@
     				<button type="button" class="navbar-toggle x collapsed" data-toggle="collapse" data-target="#navbar-collapse-x">
     					<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
     				</button>
-    					<a class="navbar-brand" href="<?= wCMS::url() ?>">
-    						<?= wCMS::get('config','siteTitle') ?>
+    					<a class="navbar-brand" href="<?= $Wcms->url() ?>">
+    						<?= $Wcms->get('config','siteTitle') ?>
 
     					</a>
     			</div>
     			<div class="collapse navbar-collapse" id="navbar-collapse-x">
     				<ul class="nav navbar-nav navbar-right">
-    					<?= wCMS::menu() ?>
+    					<?= $Wcms->menu() ?>
 
     				</ul>
     			</div>
@@ -80,28 +78,28 @@
     		<div class="row-fluid sm-gutter">
     			<div class="col-xs-12 col-sm-8">
         			<div class="box css3-shadow whiteBackground padding10">
-        				<?= wCMS::page('content') ?>
+        				<?= $Wcms->page('content') ?>
 
         			</div>
     			</div>
 
     			<div class="col-xs-12 col-sm-4">
         			<div class="box css3-shadow whiteBackground padding10">
-        				<?= wCMS::block('subside') ?>
+        				<?= $Wcms->block('subside') ?>
         			</div>
     			</div>
     		</div>
     	</div>
 
     	<footer class="container-fluid css3-shadow whiteFont colorBackground padding20">
-    		<p><?= wCMS::footer() ?></p>
+    		<p><?= $Wcms->footer() ?></p>
     	</footer>
 
         <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    	<?= wCMS::js() ?>
+    	<?= $Wcms->js() ?>
 
-    	<script src="<?= wCMS::asset('js/js.cookie.js') ?>"></script>
+    	<script src="<?= $Wcms->asset('js/js.cookie.js') ?>"></script>
     	<script>
     		$( document ).ready(function() {
     			$('body').css('margin-bottom', $('footer').height()+'px');
